8d1b922860e6a3b36bf67544cb8286e673eab582
jonathan-gateley <jonathan-gateley@codermerlin.codermerlin.com>
2022-09-12 08:21:08 -0500

    fixed stuff

--- sort.js
+++ sort.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+
 const randomWords = require('random-words');
 const question = "Enter a word or press enter to stop";
 
@@ -25,6 +26,7 @@ process.stdin.on('data', function(data) {
     } else {
 	answers.push(data.toString().trim());
 	ask();
+	
     }
     
 });
@@ -33,42 +35,94 @@ ask();
 
 function split(array){
     const half = array.length/2
-
+    process.stdout.write(array.length)
+    process.stdout.write(half)
     if (array.length < 2) {
 	return array
     }
-
+    const right= array.splice(half,half + 1)
     const left = array.splice(0,half)
-    process.stdout.write( merge(split(left),split(array)))
-}
-function sort() {
+    process.stdout.write(left[0])
+    }
 
+function sort(array) {
+    const output = []
+    var left = 0
+    var right = 1
+    while (left < array.length && right < array.length) {
+	if (compare(array[left],array[right],0)) {
+	    output.push(array[left])
+	    if (left < right) {
+		left = right + 1
+	    } else {
+		left += 1
+	    }
+	} else {
+	    output.push(array[right])
+	    process.stdout.write(output[0])
+	    if (right > left) {
+		right += 1
+	    } else {
+		right = left + 1
+	    }
+	}
+    }
+    if (left < right) {
+	output.push(array[left])
+	
+    }else{
+	output.push(array[right])
+	
+    }
+
+    for (let i = 0; i < output.length; i++) {
+	process.stdout.write(output[i])
+	if (i + 1 < output.length) {
+	    process.stdout.write(', ')
+	}
+    }
 }
+
 
 function merge(left, right) {
     let output = []
-
+    var leftWord = 0
+    var rightWord = 0 
     while (left.length > 0 && right.length > 0) {
-        if (compare(left[0], right[0], 0)) {
-            output.push(left[0])
+        if (compare(left[leftWord], right[rightWord], 0)) {
+            output.push(left[leftWord])
+	    leftWord += 1
+	    
         } else {
-            output.push(right[0])
+            output.push(right[rightWord])
+	    rightWord += 1
         }
     }
-    return output
+    for (let i = 0; i < output.length; i++) {
+	    process.stdout.write(output[i])
+	    if (i + 1 < output.length) {
+		process.stdout.write(', ')
+	    }
+	}
 }
+
 
 function compare(left, right, letter) {
     if (left == right) {
         return true
     } else if (left.charCodeAt(letter) < right.charCodeAt(letter)) {
+	process.stdout.write("true")
         return true
     } else if (left.charCodeAt(letter) > right.charCodeAt(letter)) {
+	process.stdout.write("false \n")
         return false
     } else if (left.charCodeAt(letter) == right.charCodeAt(letter)) {
+	process.stdout.write("same")
         return compare(left, right, letter + 1)
     }
+    
 }
+
 
 //var sortedwordlist = wordlist
 //console.log(sortedwordlist)
