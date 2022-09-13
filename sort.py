494a5e138c4c682c04db11d81fbe3b753c988357
Xenophon1001 <loganrohlfs76@gmail.com>
2022-09-12 21:37:42 -0500

    Added Split, Compare, and Merge functionality

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+# Take in standard user input and add it to the wordlist, can be optimized by not printing the prompt between words #
 wordlist = []
 while True:
         inp = input("Type Anything/ Press Enter: ")
@@ -9,4 +10,47 @@ while True:
         
         print(wordlist)
 
-print(ord(wordlist[0][0]))
+# Placeholder for the main sort logic to call the child functions #
+def sort(wordlist):
+        exit
+
+# Splits a list into groups of n #
+def split(wordlist, n):
+    for x in range(0, len(wordlist), n):
+        every_chunk = wordlist[x: n+x]
+
+        if len(every_chunk) < n:
+            every_chunk = every_chunk + \
+                [None for y in range(n-len(every_chunk))]
+        yield every_chunk
+
+# Merges two, pre sorted lists into one sorted list #
+def merge(left, right):
+        output = []
+        while(len(left) > 0 and len(right) > 0):
+                if (compare(left[0], right[0], 0)):
+                        output.append(left[0])
+                        left.remove(left[0])
+                else:
+                        output.append(right[0])
+                        right.remove(right[0])
+        if (len(left) > 0 and len(right) == 0):
+                output.extend(left)
+        elif (len(right) > 0 and len(left) == 0):
+                output.extend(right)
+
+        return output
+
+# Compares two words and returns true if left, false if right, and true if same as it does not matter #
+def compare(left, right, letter):
+        if (left == right):
+                return True
+
+        elif (ord(left[letter]) < ord(right[letter])):
+                return True
+
+        elif (ord(left[letter]) > ord(right[letter])):
+                return False
+        
+        elif (ord(left[letter]) == ord(right[letter])):
+                return compare(left, right, letter + 1)
--- test.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-var readline = require('readline');
-
-var words = readline()
-
-console.log(words)
